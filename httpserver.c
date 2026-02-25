e78b66f55149750a0ae389bd05e85c6451326a52
Alex Forky <boohoochicken@gmail.com>
2026-02-25 11:29:13 +0200

    Listen on socket.

---
+++ httpserver.c
@@ -0,0 +1,42 @@
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+
+
+int main(){
+    // PF_INET = IPv4 protocol
+    // The socket function can be used for creating a socket (endpoint) for any IPC, returning a file descriptor 
+    // i.e. Bluetooth, TCP, UDP etc. Thus both the domain and the type must be specified. 0 refers to the default protocol given the domain and type.
+
+    int socketfd = socket(PF_INET, SOCK_STREAM, 0);
+    if (socketfd == -1){
+        perror("An error has occured while creating the socket");
+        return -1;
+    }
+    
+    // bind() expects a sockaddr which is the general specification. sockaddr_in is for internet protocol
+    // The function associates the socket with an address.
+    struct sockaddr_in addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(80)
+    };
+    int res = inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr.s_addr);
+
+    if(res == 0 ){
+        perror("inet_pton: No valid network string for binding");
+    }
+
+    if(bind(socketfd, (struct sockaddr *)&addr, sizeof addr) == -1){
+        perror("An error has occured while binding the socket");
+        return -1;
+    };
+
+    // Now we mark the socket as accepting connections. Only necessary for stream-oriented modes (SOCK_STREAM, SOCK_SEQPACKET)
+    // Second argument represents the number of connections that can be queued up.
+    if(listen(socketfd, 10) == -1){
+        perror("Failed to listen on socket");
+    }
+
+}   
+
